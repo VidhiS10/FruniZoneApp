0849d15382623a951523a29313a25b1ebed5143a
VidhiS10 <vidhisorathia10@gmail.com>
2025-11-18 22:29:36 +0530

    sub category fragment show all details

--- FruniZone/app/src/main/java/com/example/frunizone/SubCategoryFragment.kt
+++ FruniZone/app/src/main/java/com/example/frunizone/SubCategoryFragment.kt
@@ -5,11 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
+import com.example.frunizone.model.FurnitureModel
+import java.time.LocalDate
+import java.time.LocalTime
+import com.example.frunizone.util.ConstantData
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+
+import android.widget.Toast
+
+import com.example.frunizone.api.OrderApi
+import com.example.frunizone.model.OrderModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+private lateinit var subCategorySlider: ImageSlider
+private lateinit var subColor: TextView
+private lateinit var subName: TextView
+private lateinit var subRating: TextView
+private lateinit var subDimension: TextView
+private lateinit var subWarranty: TextView
+private lateinit var subWeight: TextView
+private lateinit var subMaterial: TextView
+private lateinit var subSku: TextView
+private lateinit var subSaveRupees: TextView
+private lateinit var subRupees: TextView
+private lateinit var subDescription: TextView
+private lateinit var subSpecification: TextView
+private lateinit var btnSubCart: Button
 
 /**
  * A simple [Fragment] subclass.
@@ -36,7 +69,84 @@ class SubCategoryFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sub_category, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val furnitureModel = arguments?.getSerializable("product") as FurnitureModel
+
+        subCategorySlider = view.findViewById(R.id.sub_category_slider)
+        subColor = view.findViewById(R.id.sub_color)
+        subName = view.findViewById(R.id.sub_name)
+        subRating = view.findViewById(R.id.Sub_rating)
+        subDimension = view.findViewById(R.id.sub_dimention)
+        subWarranty = view.findViewById(R.id.sub_warranty)
+        subWeight = view.findViewById(R.id.sub_wieght)
+        subMaterial = view.findViewById(R.id.sub_material)
+        subSku = view.findViewById(R.id.sub_sku)
+        subSaveRupees = view.findViewById(R.id.sub_saverupees)
+        subRupees = view.findViewById(R.id.sub_rupees)
+        subDescription = view.findViewById(R.id.sub_description)
+        subSpecification = view.findViewById(R.id.sub_specification)
+        btnSubCart = view.findViewById(R.id.btn_sub_cart)
+
+        // set data
+        subColor.text = furnitureModel.sub_cat_color
+        subName.text = furnitureModel.sub_cat_name
+        subRating.text = furnitureModel.sub_cat_product_rating
+        subDimension.text = furnitureModel.sub_cat_dimention
+        subWarranty.text = furnitureModel.sub_cat_warenty
+        subWeight.text = furnitureModel.sub_cat_weight
+        subMaterial.text = furnitureModel.sub_cat_primary_material
+        subSku.text = furnitureModel.sub_cat_sku
+        subSaveRupees.text = furnitureModel.sub_cat_discount
+        subRupees.text = furnitureModel.sub_cat_price
+        subDescription.text = furnitureModel.sub_cat_description
+        subSpecification.text = furnitureModel.sub_cat_specification
+
+        // Slider
+        val list = arrayListOf(
+            SlideModel(
+                ConstantData.SERVER_IMAGE_ADDRESS + furnitureModel.sub_cat_pic1,
+                ScaleTypes.CENTER_CROP
+            ),
+            SlideModel(ConstantData.SERVER_IMAGE_ADDRESS + furnitureModel.sub_cat_pic2, ScaleTypes.CENTER_CROP),
+            SlideModel(ConstantData.SERVER_IMAGE_ADDRESS + furnitureModel.sub_cat_pic3, ScaleTypes.CENTER_CROP),
+            SlideModel(ConstantData.SERVER_IMAGE_ADDRESS + furnitureModel.sub_cat_pic4, ScaleTypes.CENTER_CROP)
+        )
+
+        subCategorySlider.setImageList(list)
+
+        btnSubCart.setOnClickListener {
+            val sp = requireActivity().getSharedPreferences(ConstantData.SP_LOGIN_PREFS, Context.MODE_PRIVATE)
+            val uid = sp.getString(ConstantData.KEY_ID, "0") ?: "0"
+
+            val currentDate = LocalDate.now()
+            val currentTime = LocalTime.now()
+
+            if (uid == "0") {
+                Toast.makeText(requireContext(), "Login to Continue", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(requireActivity(), LoginActivity::class.java))
+            } else {
+                val orderModel = OrderModel(
+                    "",
+                    uid,
+                    furnitureModel.sub_cat_id,
+                    furnitureModel.sub_cat_name,
+                    furnitureModel.sub_cat_pic1,
+                    furnitureModel.sub_cat_price,
+                    furnitureModel.sub_cat_price,
+                    "0",
+                    "1",
+                    currentDate.toString(),
+                    currentTime.toString(),
+                    "0",
+                    ""
+                )
+
+                OrderApi().addOrder(orderModel, requireActivity())
+            }
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
--- FruniZone/app/src/main/java/com/example/frunizone/api/OrderApi.kt
+++ FruniZone/app/src/main/java/com/example/frunizone/api/OrderApi.kt
@@ -1,4 +1,276 @@
 package com.example.frunizone.api
 
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.frunizone.CartActivity
+import com.example.frunizone.HomeActivity
+import com.example.frunizone.YourOrderFragment
+import com.example.frunizone.model.OrderModel
+import com.example.frunizone.model.OrderOutputModel
+import com.example.frunizone.util.ConstantData
+import com.google.gson.Gson
+
 class OrderApi {
+
+    fun addOrder(model: OrderModel, activity: Activity) {
+
+        val progress = ProgressDialog(activity)
+        progress.setMessage("Its loading....")
+        progress.setTitle("Fetching Data")
+        progress.show()
+
+        val url = ConstantData.SERVER_ADDRESS + ConstantData.ORDER_URL
+        val queue: RequestQueue = Volley.newRequestQueue(activity)
+
+        val request = object : StringRequest(Request.Method.POST, url,
+            { response ->
+                progress.dismiss()
+                val output = Gson().fromJson(response, OrderOutputModel::class.java)
+                Toast.makeText(activity, output.message, Toast.LENGTH_SHORT).show()
+            },
+            { error ->
+                progress.dismiss()
+                Toast.makeText(activity, "Error: ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }) {
+
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val map = HashMap<String, String>()
+                map["uid"] = model.uid
+                map["pid"] = model.pid
+                map["pname"] = model.pname
+                map["ppic"] = model.ppic
+                map["date"] = model.date
+                map["time"] = model.time
+                map["amount"] = model.amount
+                map["total_amount"] = model.totalAmount
+                map["quantity"] = model.quantity + "1"
+                map["status"] = model.status
+                map["is_wishlist"] = model.isWishlist
+                return map
+            }
+        }
+
+        queue.add(request)
+    }
+
+   /* fun removeOrder(id: String, activity: Activity) {
+
+        val progress = ProgressDialog(activity)
+        progress.setMessage("Its loading....")
+        progress.setTitle("Fetching Data")
+        progress.show()
+
+        val url = ConstantData.SERVER_ADDRESS + ConstantData.REMOVE_ORDER
+        val queue = Volley.newRequestQueue(activity)
+
+        val request = object : StringRequest(Request.Method.POST, url,
+            { response ->
+                progress.dismiss()
+                val output = Gson().fromJson(response, OrderOutputModel::class.java)
+                Toast.makeText(activity, output.message, Toast.LENGTH_SHORT).show()
+
+                if (output.status) {
+                    (activity as CartActivity).getCartData()
+                }
+            },
+            { error ->
+                progress.dismiss()
+                Toast.makeText(activity, "Error: ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }) {
+
+            override fun getParams(): Map<String, String> {
+                return hashMapOf("id" to id)
+            }
+        }
+
+        queue.add(request)
+    }*/
+
+    fun cancelOrder(id: String, activity: Activity) {
+
+        val progress = ProgressDialog(activity)
+        progress.setMessage("Its loading....")
+        progress.setTitle("Fetching Data")
+        progress.show()
+
+        val url = ConstantData.SERVER_ADDRESS + ConstantData.CANCLE_ORDER
+        val queue = Volley.newRequestQueue(activity)
+
+        val request = object : StringRequest(Request.Method.POST, url,
+            { response ->
+                progress.dismiss()
+                val output = Gson().fromJson(response, OrderOutputModel::class.java)
+                Toast.makeText(activity, output.message, Toast.LENGTH_SHORT).show()
+
+                if (output.status) {
+                    val intent = Intent(activity, HomeActivity::class.java)
+                    activity.startActivity(intent)
+                }
+            },
+            { error ->
+                progress.dismiss()
+                Toast.makeText(activity, "Error: ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }) {
+
+            override fun getParams(): Map<String, String> {
+                return hashMapOf("id" to id)
+            }
+        }
+
+        queue.add(request)
+    }
+/*
+    fun confirmOrder(uid: String, address: String, status: String, activity: Activity) {
+
+        val progress = ProgressDialog(activity)
+        progress.setMessage("Its loading....")
+        progress.setTitle("Fetching Data")
+        progress.show()
+
+        val url = ConstantData.SERVER_ADDRESS + ConstantData.CONFIRM_ORDER
+        val queue = Volley.newRequestQueue(activity)
+
+        val request = object : StringRequest(Request.Method.POST, url,
+            { response ->
+                progress.dismiss()
+                val output = Gson().fromJson(response, OrderOutputModel::class.java)
+                Toast.makeText(activity, output.message, Toast.LENGTH_SHORT).show()
+
+                if (output.status) {
+                    val cart = activity as CartActivity
+                    cart.getCartData()
+                    cart.done()
+                }
+            },
+            { error ->
+                progress.dismiss()
+                Toast.makeText(activity, "Error: ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }) {
+
+            override fun getParams(): Map<String, String> {
+                val map = HashMap<String, String>()
+                map["uid"] = uid
+                map["address"] = address
+                map["status"] = status
+                return map
+            }
+        }
+
+        queue.add(request)
+    }*/
+/*
+    fun updateOrder(id: String, qty: String, amount: String, activity: Activity) {
+
+        val progress = ProgressDialog(activity)
+        progress.setMessage("Its loading....")
+        progress.setTitle("Fetching Data")
+        progress.show()
+
+        val url = ConstantData.SERVER_ADDRESS + ConstantData.UPDATE_ORDER
+        val queue = Volley.newRequestQueue(activity)
+
+        val request = object : StringRequest(Request.Method.POST, url,
+            { response ->
+                progress.dismiss()
+                val output = Gson().fromJson(response, OrderOutputModel::class.java)
+
+                if (output.status) {
+                    (activity as CartActivity).getCartData()
+                }
+                Toast.makeText(activity, output.message, Toast.LENGTH_SHORT).show()
+            },
+            { error ->
+                progress.dismiss()
+                Toast.makeText(activity, "Error: ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }) {
+
+            override fun getParams(): Map<String, String> {
+                return hashMapOf(
+                    "id" to id,
+                    "amount" to amount,
+                    "quantity" to qty
+                )
+            }
+        }
+
+        queue.add(request)
+    }
+
+    fun getOrderPending(uid: String, activity: Activity) {
+
+        val progress = ProgressDialog(activity)
+        progress.setMessage("Its loading....")
+        progress.setTitle("Fetching Data")
+        progress.show()
+
+        val url = ConstantData.SERVER_ADDRESS + ConstantData.GET_PENDING_ORDER_URL
+        val queue = Volley.newRequestQueue(activity)
+
+        val request = object : StringRequest(Request.Method.POST, url,
+            { response ->
+                progress.dismiss()
+                val output = Gson().fromJson(response, OrderOutputModel::class.java)
+                Toast.makeText(activity, output.message, Toast.LENGTH_SHORT).show()
+
+                if (output.status) {
+                    (activity as CartActivity).setCart(output)
+                }
+            },
+            { error ->
+                progress.dismiss()
+                Toast.makeText(activity, "Error: ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }) {
+
+            override fun getParams(): Map<String, String> {
+                return hashMapOf("uid" to uid)
+            }
+        }
+
+        queue.add(request)
+    }
+*/
+    /*
+    fun getOrderCompleted(uid: String, fragment: YourOrderFragment) {
+
+        val activity = fragment.requireActivity()
+        val progress = ProgressDialog(activity)
+        progress.setMessage("Its loading....")
+        progress.setTitle("Fetching Data")
+        progress.show()
+
+        val url = ConstantData.SERVER_ADDRESS + ConstantData.COMPLETED_ORDER
+        val queue = Volley.newRequestQueue(activity)
+
+        val request = object : StringRequest(Request.Method.POST, url,
+            { response ->
+                progress.dismiss()
+                val output = Gson().fromJson(response, OrderOutputModel::class.java)
+
+                Toast.makeText(activity, output.message, Toast.LENGTH_SHORT).show()
+
+                if (output.status) {
+                    fragment.getOrderHistory(output)
+                }
+            },
+            { error ->
+                progress.dismiss()
+                Toast.makeText(activity, "Error: ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }) {
+
+            override fun getParams(): Map<String, String> {
+                return hashMapOf("uid" to uid)
+            }
+        }
+
+        queue.add(request)
+    }*/
 }
